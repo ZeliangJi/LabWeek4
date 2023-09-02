c2d2278114cf1e339c03a33013ef5043799d6a02
Zeliang Ji <Zeliang.Ji-1@student.uts.edu.au>
2023-09-02 20:19:01 +1000

    Rename ConsolePrint to PrintAndHide, set rend to public, and update tags

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
-    private int i = 3; 
-
+    private int frameNumber = 3;
+    public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +15,7 @@ public class ConsolePrint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gameObject.name + ":" + i); 
-        i++;
+        frameNumber++;
+        Debug.Log(gameObject.name + ":" + frameNumber);
     }
 }
